27b9d122e621028d06286341993bab326df1673e
Joaquín Ortiz <joacoortizmartinez@gmail.com>
2024-07-25 12:47:33 -0300

    Cambios

---
+++ SGSYF_main/SGSYF_clases/Guardar_datos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SGSYF_clases
+{
+    public class Guardar_datos
+    {
+        private string nombre_usuario;
+
+        
+
+        public string Nombre_usuario { get => nombre_usuario; set => nombre_usuario = value; }
+    }
+}
