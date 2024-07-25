3a502f00bb37f09169292ba35d9098345be9e645
joacoortizmartinez <joacoortizmartinez@gmail.com>
2024-07-25 12:48:57 -0300

    Data Base actualizadaa

---
+++ SGSYF_main/gestor_stock/gestor_stock (3).sql
@@ -0,0 +1,227 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-07-2024 a las 17:48:37
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `gestor_stock`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categorias`
+--
+
+CREATE TABLE `categorias` (
+  `id_categoria` int(11) NOT NULL,
+  `nombre` char(100) NOT NULL,
+  `descripcion` char(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `categorias`
+--
+
+INSERT INTO `categorias` (`id_categoria`, `nombre`, `descripcion`) VALUES
+(1, 't', 't'),
+(2, 'tdddddddd', 't'),
+(3, 'tdddddddd', ''),
+(4, 'dsfsdf', ''),
+(5, 'dfg', 'dfg'),
+(6, 'dfgdfgdfg', 'dfg'),
+(7, 'lkñ', ''),
+(8, 'ryuk', ''),
+(9, 'ryuk', ''),
+(10, 'aderezos', ''),
+(11, 'mayonesas', ''),
+(12, 'sdf', 'sdf'),
+(13, 'Intento1', '');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `configuracion`
+--
+
+CREATE TABLE `configuracion` (
+  `id_config` int(11) NOT NULL,
+  `nombre_empresa` char(150) DEFAULT NULL,
+  `logo` char(250) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `configuracion`
+--
+
+INSERT INTO `configuracion` (`id_config`, `nombre_empresa`, `logo`) VALUES
+(1, 'NombreDeTuEmpresa', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `codigo_barra` int(11) NOT NULL,
+  `categoria` char(50) DEFAULT NULL,
+  `subcategoria` char(50) DEFAULT NULL,
+  `nombre` char(50) DEFAULT NULL,
+  `descripcion` char(100) DEFAULT NULL,
+  `precio` int(11) DEFAULT NULL,
+  `unidad_medida` char(50) DEFAULT NULL,
+  `precio_compra` int(11) DEFAULT NULL,
+  `stock_total` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`codigo_barra`, `categoria`, `subcategoria`, `nombre`, `descripcion`, `precio`, `unidad_medida`, `precio_compra`, `stock_total`) VALUES
+(1, 'f', 'f', 'papin', 'papas lays', 3, 'f', 3, 100),
+(2, 'mayonesas', 's', 'mayonesa', 'hellmans', 1000, 'kilo', 200, 20);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `subcategorias`
+--
+
+CREATE TABLE `subcategorias` (
+  `id_subcategoria` int(11) NOT NULL,
+  `nombre` char(100) NOT NULL,
+  `descripcion` char(150) DEFAULT NULL,
+  `id_categoria` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `subcategorias`
+--
+
+INSERT INTO `subcategorias` (`id_subcategoria`, `nombre`, `descripcion`, `id_categoria`) VALUES
+(3, 's', '', 11),
+(4, 'mayonesas grandes', '', 11),
+(8, 's', '', 5),
+(9, 's', '', 11),
+(10, 'grrrrrrande', '', 11),
+(11, 'IntentoSub1', '', 13);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(150) NOT NULL,
+  `contraseña` varchar(150) NOT NULL,
+  `palabra` varchar(150) DEFAULT NULL,
+  `tipo_usuario` varchar(150) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `usuario`, `contraseña`, `palabra`, `tipo_usuario`) VALUES
+(2, 'Admin', 'joaco123', 'SoloYoPuedo', 'Administrador'),
+(3, 'JoacoModoUsuario', 'aaaaa', NULL, 'Usuario'),
+(4, 'aaaaaa', 'aaa', NULL, 'Usuario'),
+(5, 'achange', 'aaaa', NULL, 'Usuario'),
+(6, 'a', 'a', NULL, 'Usuario'),
+(7, 'a', 'a', NULL, 'Usuario');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `configuracion`
+--
+ALTER TABLE `configuracion`
+  ADD PRIMARY KEY (`id_config`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`codigo_barra`);
+
+--
+-- Indices de la tabla `subcategorias`
+--
+ALTER TABLE `subcategorias`
+  ADD PRIMARY KEY (`id_subcategoria`),
+  ADD KEY `id_categoria` (`id_categoria`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `categorias`
+--
+ALTER TABLE `categorias`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT de la tabla `configuracion`
+--
+ALTER TABLE `configuracion`
+  MODIFY `id_config` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `subcategorias`
+--
+ALTER TABLE `subcategorias`
+  MODIFY `id_subcategoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `subcategorias`
+--
+ALTER TABLE `subcategorias`
+  ADD CONSTRAINT `subcategorias_ibfk_1` FOREIGN KEY (`id_categoria`) REFERENCES `categorias` (`id_categoria`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
